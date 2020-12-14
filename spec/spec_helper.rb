936d2f33fabfcbd92a0086511751f7a4d3c2b61a
ollienorman <oliver.norman1991@gmail.com>
2020-12-14 14:34:37 +0000

    rubocop and simplecov running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,13 @@ require 'simplecov'
 require 'simplecov-console'
 require_relative '../app.rb'
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 
 Capybara.app = BookmarkManager
