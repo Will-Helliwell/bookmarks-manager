990efee01fdbc41afbbd5095d54b6e71be900703
Ian Fleming <>
2020-12-16 16:46:24 +0000

    test environment setup

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ I would like to see a list of my bookmarks
 2. Create the database using the psql command 'CREATE DATABASE bookmark_manager;'
 3. Connect to the database using the psql command '\c bookmark_manager;'
 4. Run the query we have saved in the file '01_create_bookmarks_table.sql' (in the db/migrations directory.)
+
+
+# Test Database
+
+1. set up a new database called bookmarks_manager_test
+2. set up a new table within this database and give it the exact same name as your original table
+3. Enter in all of the same URL links found in your original table
--- spec/bookmarkfolder_spec.rb
+++ spec/bookmarkfolder_spec.rb
@@ -1,18 +1,18 @@
 require 'bookmarkfolder'
 
-  describe BookmarkFolder do
-    before(:each) do
-      empty
-      add_base_bookmarks
-    end
-    describe '#all' do
-      it 'returns a list of bookmarks' do
-        folder = BookmarkFolder.new
-        bookmarks = folder.all
+describe BookmarkFolder do
+  before(:each) do
+    empty
+    add_base_bookmarks
+  end
+  describe '#all' do
+    it 'returns a list of bookmarks' do
+      folder = BookmarkFolder.new
+      bookmarks = folder.all
 
-        expect(bookmarks).to include "http://www.makersacademy.com"
-        expect(bookmarks).to include "http://www.destroyallsoftware.com"
-        expect(bookmarks).to include "http://www.google.com"
-      end
+      expect(bookmarks).to include "http://www.makersacademy.com"
+      expect(bookmarks).to include "http://www.destroyallsoftware.com"
+      expect(bookmarks).to include "http://www.google.com"
     end
   end
+end
